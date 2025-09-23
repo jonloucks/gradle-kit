61ca152ef2ab398e7e53925ae4c6c8ff23820a40
Jon Loucks <j.loucks@comcast.net>
2025-09-23 17:18:26 -0400

    Explicit Jacoco dependencies

--- src/main/java/io/github/jonloucks/gradle/kit/JacocoApplier.java
+++ src/main/java/io/github/jonloucks/gradle/kit/JacocoApplier.java
@@ -5,6 +5,8 @@ import org.gradle.api.Project;
 import org.gradle.api.file.DirectoryProperty;
 import org.gradle.api.tasks.SourceSet;
 import org.gradle.api.tasks.SourceSetContainer;
+import org.gradle.api.tasks.TaskCollection;
+import org.gradle.api.tasks.testing.Test;
 import org.gradle.testing.jacoco.plugins.JacocoPlugin;
 import org.gradle.testing.jacoco.plugins.JacocoPluginExtension;
 import org.gradle.testing.jacoco.tasks.JacocoReport;
@@ -32,22 +34,9 @@ final class JacocoApplier {
             .configure(configureExistingReport());
     }
     
-    private @NotNull Action<@NotNull Project> getAllJacocoFiles(JacocoReport rootReport) {
-        return project -> {
-            final SourceSetContainer sourceSets = project.getExtensions().getByType(SourceSetContainer.class);
-            final SourceSet sourceSet = sourceSets.getByName("main");
-            final DirectoryProperty buildDir = project.getLayout().getBuildDirectory();
-            rootReport.getInputs().getSourceFiles().plus(sourceSet.getJava());
-            rootReport.getAdditionalSourceDirs().from(sourceSet.getJava().getSrcDirs());
-            rootReport.getAdditionalClassDirs().from(sourceSet.getOutput().getClassesDirs());
-            rootReport.getExecutionData().from(project.fileTree(buildDir.dir("jacoco")).include("**.exec"));
-        };
-    }
-    
     private void configureJacocoPlugin() {
         targetProject.getPlugins().withType(JacocoPlugin.class, jacocoPlugin -> {
             JacocoPluginExtension jacocoExtension = targetProject.getExtensions().getByType(JacocoPluginExtension.class);
-            // Configure the toolVersion here
             jacocoExtension.setToolVersion("0.8.13");
         });
     }
@@ -56,8 +45,9 @@ final class JacocoApplier {
         final boolean isRootProject = isRootProject(targetProject);
         
         return reportTask -> {
-            reportTask.shouldRunAfter("test", "integrationTest", "functionalTest");
-            reportTask.dependsOn("test", "integrationTest", "functionalTest");
+            final TaskCollection<@NotNull Test> testingTasks = getTestingTasks(targetProject);
+            reportTask.shouldRunAfter(testingTasks);
+            reportTask.dependsOn(testingTasks);
             reportTask.reports(reports -> {
                 reports.getHtml().getRequired().set(isRootProject);
                 reports.getXml().getRequired().set(true);
@@ -67,6 +57,35 @@ final class JacocoApplier {
                 targetProject.allprojects(getAllJacocoFiles(reportTask));
             }
         };
+    }
+    
+    private @NotNull Action<@NotNull Project> getAllJacocoFiles(JacocoReport rootReport) {
+        final Project rootProject = targetProject;
+        return project -> {
+            final TaskCollection<@NotNull Test> testingTasks = getTestingTasks(project);
+            rootReport.shouldRunAfter(testingTasks);
+            rootReport.dependsOn(testingTasks);
+            
+            final SourceSetContainer sourceSets = project.getExtensions().getByType(SourceSetContainer.class);
+            final SourceSet sourceSet = sourceSets.getByName("main");
+            final DirectoryProperty buildDir = project.getLayout().getBuildDirectory();
+            rootReport.getAdditionalSourceDirs().from(sourceSet.getJava().getSrcDirs());
+            rootReport.getAdditionalClassDirs().from(sourceSet.getOutput().getClassesDirs());
+            rootReport.getExecutionData().from(project.fileTree(buildDir.dir("jacoco")).include("**.exec"));
+        };
+    }
+    
+    private static TaskCollection<@NotNull Test> getTestingTasks(Project project) {
+        return project.getTasks().withType(Test.class).matching(t -> {
+            switch (t.getName()) {
+                case "test":
+                case "integrationTest":
+                case "functionalTest":
+                    return true;
+                default:
+                    return false;
+            }
+        });
     }
     
     private static final String JACOCO_TEST_REPORT = "jacocoTestReport";
